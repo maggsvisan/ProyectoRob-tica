82899cca16c51a017419b69a5e40e40a03a091ff
JoNRS <jonathanrosassandoval@gmail.com>
2017-02-26 19:00:33 -0600

    Blinker c/timer1 funcional

--- codigoparamaggieconcariñodepartedemi.ino
+++ codigoparamaggieconcariñodepartedemi.ino
@@ -1,35 +1,59 @@
-const int buttonPin1 = 2;     // the number of the pushbutton pin
-const int ledPin1 =  9;      // the number of the LED pin
-
-const int buttonPin2 = 3;     // the number of the pushbutton pin
-const int ledPin2 =  8;      // the number of the LED pin
-// variables will change:
-volatile int buttonState1 = 0;         // variable for reading the pushbutton status
-volatile int buttonState2 = 0;         // variable for reading the pushbutton status
-
-void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin1, OUTPUT);
-  pinMode(ledPin2, OUTPUT);
-  
-  // initialize the pushbutton pin as an input:
-  pinMode(buttonPin1, INPUT);
-  pinMode(buttonPin2, INPUT);
-  // Attach an interrupt to the ISR vector
-  attachInterrupt(digitalPinToInterrupt(buttonPin1), pin_ISR1, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(buttonPin2), pin_ISR2, CHANGE);
+#include <TimerOne.h>
+volatile int count=0;
+volatile int buttonState;
+volatile int  state1,state2;
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(2,INPUT);
+  pinMode(9,OUTPUT);
+  pinMode(8,OUTPUT);
+  digitalWrite(9,LOW);
+  // Initialize the digital pin as an output.
+  // Pin 13 has an LED connected on most Arduino boards
+  Timer1.initialize(100000); // set a timer of length 100000 microseconds (or 0.1 sec - or 10Hz => the led will blink 5 times, 5 cycles of on-and-off, per second)
+  Timer1.attachInterrupt( timerIsr ); // attach the service routine here
 }
-
-void loop() {
-  // Nothing here!
+ 
+void loop()
+{
+ unsigned long copia,copia2;
+ noInterrupts();
+ copia=!buttonState;
+ copia2=count;
+ interrupts();
+ Serial.println(copia);
+ Serial.println(copia2);
 }
-
-void pin_ISR1() {
-  buttonState1 = digitalRead(buttonPin1);
-  digitalWrite(ledPin1, buttonState1);
-}
-
-void pin_ISR2() {
-  buttonState2 = digitalRead(buttonPin2);
-  digitalWrite(ledPin2, buttonState2);
+ 
+/// --------------------------
+/// Custom ISR Timer Routine
+/// --------------------------
+void timerIsr()
+{
+  state1=!state1;
+  digitalWrite(8,state1);
+  buttonState=digitalRead(2);
+  if(!buttonState){
+    count = count+1;
+    if(count <=5){
+      state2=HIGH;
+      digitalWrite(9,state2);
+    }
+    else if(count <=7){
+      state2=LOW;
+      digitalWrite(9,state2);
+    }
+    else{
+      count=0;
+      state2=LOW;
+      digitalWrite(9,state2);
+    }
+   }
+   else{
+     count=0;
+     state2=LOW;
+     digitalWrite(9,state2);
+   }
+     
 }
