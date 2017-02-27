84ad4ad76b0944583d52b97610d34c76aeec3b38
JoNRS <jonathanrosassandoval@gmail.com>
2017-02-26 19:20:35 -0600

    Dos botones funcionales!!!!!
    Falta juntar todo

--- codigoparamaggieconcariñodepartedemi.ino
+++ codigoparamaggieconcariñodepartedemi.ino
@@ -1,14 +1,15 @@
 #include <TimerOne.h>
-volatile int count=0;
-volatile int buttonState;
+volatile int count=0, count2=0;;
+volatile int buttonState, buttonState2;;
 volatile int  state1,state2;
 void setup() 
 {
-  Serial.begin(9600);
   pinMode(2,INPUT);
+  pinMode(3,INPUT);
   pinMode(9,OUTPUT);
   pinMode(8,OUTPUT);
   digitalWrite(9,LOW);
+  digitalWrite(8,LOW);
   // Initialize the digital pin as an output.
   // Pin 13 has an LED connected on most Arduino boards
   Timer1.initialize(100000); // set a timer of length 100000 microseconds (or 0.1 sec - or 10Hz => the led will blink 5 times, 5 cycles of on-and-off, per second)
@@ -17,13 +18,6 @@ void setup()
  
 void loop()
 {
- unsigned long copia,copia2;
- noInterrupts();
- copia=!buttonState;
- copia2=count;
- interrupts();
- Serial.println(copia);
- Serial.println(copia2);
 }
  
 /// --------------------------
@@ -31,8 +25,6 @@ void loop()
 /// --------------------------
 void timerIsr()
 {
-  state1=!state1;
-  digitalWrite(8,state1);
   buttonState=digitalRead(2);
   if(!buttonState){
     count = count+1;
@@ -54,6 +46,29 @@ void timerIsr()
      count=0;
      state2=LOW;
      digitalWrite(9,state2);
+   }
+
+    buttonState2=digitalRead(3);
+  if(!buttonState2){
+    count2 = count2+1;
+    if(count2 <=5){
+      state1=HIGH;
+      digitalWrite(8,state1);
+    }
+    else if(count2 <=7){
+      state1=LOW;
+      digitalWrite(8,state1);
+    }
+    else{
+      count2=0;
+      state1=LOW;
+      digitalWrite(8,state1);
+    }
+   }
+   else{
+     count2=0;
+     state1=LOW;
+     digitalWrite(8,state1);
    }
      
 }
